8f7d4cd914c2d1e01f8a47879f886f0a395d4e66
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-20 16:15:48 +0800

    Create remove_duplicate.cpp

---
+++ remove_duplicate.cpp
@@ -0,0 +1,37 @@
+# Remove_duplicate
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution 
+{
+public:
+	vector<int> count;
+	vector<int> length;
+	vector<int> removeDuplicates(vector<int> nums) 
+	{
+		for (int i = 0; i < nums.size(); i++)
+		{
+			count.push_back(nums[i]);
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[j] == nums[i])
+					nums.erase(nums.begin()+j);
+			}
+		}
+
+		for (int k = 0; k < count.size(); k++)
+		{
+			length.push_back(k + 1);
+			cout << length[k];
+		}
+			
+		return length;
+	}
+};
+
+void main()
+{
+	Solution solution;
+	solution.removeDuplicates({ 1,1,2});
+}
