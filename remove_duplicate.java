e585306adaa1c1d3784647726a5cc3d0de732cdb
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-20 16:49:47 +0800

    Create remove_duplicate.java

---
+++ remove_duplicate.java
@@ -0,0 +1,10 @@
+package remove_duplicate;
+
+public class remove_duplicate {
+public static void main (String[] args)
+{
+	solution Solution=new solution();
+	int[] array={1,1,2};
+	Solution.remove(array);
+}
+}
